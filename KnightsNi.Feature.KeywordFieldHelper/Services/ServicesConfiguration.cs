5479737fa980621fcf13fb72da44ac86d0ff3911
Alexander Kay <alexander.kay@apiqu.com>
2019-03-02 02:11:37 -0800

    services configurations

---
+++ KnightsNi.Feature.KeywordFieldHelper/Services/ServicesConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace Knights.Feature.KeywordFieldHelper.Services
+{
+    public class ServicesConfiguration : IServicesConfigurator
+    {
+        #region Implementation of IServicesConfigurator
+
+        public void Configure(IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddMvcControllers("Knights.*");
+        }
+
+        #endregion
+    }
+}
