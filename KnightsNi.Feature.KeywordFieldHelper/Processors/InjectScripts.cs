442ce3132b26a63f4920787df07ddb5e2989f787
Dennis <dennis@6ixshooters.com>
2019-03-01 20:20:35 -0800

    Initial project NI!

---
+++ KnightsNi.Feature.KeywordFieldHelper/Processors/InjectScripts.cs
@@ -0,0 +1,38 @@
+﻿using Sitecore.Diagnostics;
+using Sitecore.Pipelines;
+using Sitecore.StringExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+
+namespace Knights.Feature.KeywordFieldHelper.Processors
+{
+    public class InjectScripts
+    {
+        private const string JavascriptTag = "<script src=\"{0}\"></script>";
+        private const string StylesheetLinkTag = "<link href=\"{0}\" rel=\"stylesheet\" />";
+
+        public void Process(PipelineArgs args)
+        {
+            AddControls(JavascriptTag, "CustomContentEditorJavascript");
+            AddControls(StylesheetLinkTag, "CustomContentEditorStylesheets");
+        }
+
+        private void AddControls(string resourceTag, string configKey)
+        {
+            Assert.IsNotNullOrEmpty(configKey, "Content Editor resource config key cannot be null");
+
+            string resources = Sitecore.Configuration.Settings.GetSetting(configKey);
+
+            if (String.IsNullOrEmpty(resources))
+                return;
+
+            foreach (var resource in resources.Split('|'))
+            {
+                Sitecore.Context.Page.Page.Header.Controls.Add((Control)new LiteralControl(resourceTag.FormatWith(resource)));
+            }
+        }
+    }
+}
---
+++ KnightsNi.Feature.KeywordFieldHelper/sitecore modules/KeywordFieldHelper/keywordfieldhelper.js
@@ -0,0 +1,9 @@
+﻿
+(function ($) {
+    $(document).ready(function () {
+
+
+        alert("NI!");
+
+    });
+})(jQuery);
